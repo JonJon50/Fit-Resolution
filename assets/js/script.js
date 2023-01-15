12e1c77d35e5ed96f13fbe02280359a873ac1ba4
tanglero4 <tiffanyanglero94@hotmail.com>
2023-01-15 18:14:43 -0500

    Added starter code for google maps API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,99 +1,90 @@
-let startBtn = document.querySelector(".start-btn")
-let mainSection = document.querySelector(".section")
-let questionsArea = document.querySelector(".questions-box")
-let questionsText = document.querySelector(".questions-area")
-let answerTextOne = document.querySelector(".answer-area1")
-let answerTextTwo = document.querySelector(".answer-area2")
-let answerBtn = document.querySelector(".answer")
-
-
+let startBtn = document.querySelector(".start-btn");
+let mainSection = document.querySelector(".section");
+let questionsArea = document.querySelector(".questions-box");
+let questionsText = document.querySelector(".questions-area");
+let answerTextOne = document.querySelector(".answer-area1");
+let answerTextTwo = document.querySelector(".answer-area2");
+let answerBtn = document.querySelector(".answer");
 
 let questionsIndex = 0;
 
 let questions = [
-    {
+  {
     question: "How would you describe your current fitness level?",
     answer1: "Beginning of my journey",
     answer2: "Somewhat active",
-    },
-    {
+  },
+  {
     question: "What is your biggest fitness struggle right now?",
     answer1: "Scheduling my workouts",
     answer2: "Support and accountability",
-    },
-    {
+  },
+  {
     question: "What inspires you to workout?",
     answer1: "Taking care of myself",
     answer2: "Reaching my peak performance",
-    },
-    {
+  },
+  {
     question: "What is your primary fitness goal?",
     answer1: "Stay healthy",
-    answer2: "Get stronger",    
-    },
-    {
+    answer2: "Get stronger",
+  },
+  {
     question: "What quote do you relate to most?",
     answer1: "You're only one workout away from a good mood - Unknown",
     answer2: "The body achieves what the mind believes - Unknown",
-    },
-    {
-question: "Which of the following would you be most interested in trying?",
-answer1: "Mobility, flexibility and alternative workouts",
-answer2: "Endurance & weight lifting",
-    }
+  },
+  {
+    question: "Which of the following would you be most interested in trying?",
+    answer1: "Mobility, flexibility and alternative workouts",
+    answer2: "Endurance & weight lifting",
+  },
 ];
 
+startBtn.addEventListener("click", function () {
+  startQuestions();
+});
 
-startBtn.addEventListener('click', function () {
- startQuestions()
-})
+function startQuestions() {
+  questionsArea.classList.remove("is-hidden");
+  mainSection.classList.add("is-hidden");
 
-function startQuestions (){
-    questionsArea.classList.remove("is-hidden")
-    mainSection.classList.add("is-hidden")
-
-if(questionsIndex === questions.length){
-    endQuestions()
-} else {
+  if (questionsIndex === questions.length) {
+    endQuestions();
+  } else {
     questionsText.innerHTML = questions[questionsIndex].question;
     answerTextOne.innerHTML = questions[questionsIndex].answer1;
     answerTextTwo.innerHTML = questions[questionsIndex].answer2;
-}
-}
-
-function confirmAnswer(answer){
-    let usersAnswer = questions[questionsIndex];
-
-    if(answer === usersAnswer.answer1){
-        questionsIndex++;
-        startQuestions()
-    }
-    else if (answer !== usersAnswer.answer1);
+  }
 }
 
+function confirmAnswer(answer) {
+  let usersAnswer = questions[questionsIndex];
 
+  if (answer === usersAnswer.answer1) {
+    questionsIndex++;
+    startQuestions();
+  } else if (answer !== usersAnswer.answer1);
+}
 
 answerBtn.addEventListener("click", (event) => {
-const answer = event.target.innerText;
-confirmAnswer(answer);
+  const answer = event.target.innerText;
+  confirmAnswer(answer);
+});
 
-}
-)
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "dfbd975756msh5c34e78571a6059p156249jsne62ed8b440fa",
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
 
-      const options = {
-          method: 'GET',
-          headers: {
-              'X-RapidAPI-Key': 'dfbd975756msh5c34e78571a6059p156249jsne62ed8b440fa',
-              'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-          }
-      };
-      
-      fetch('https://exercisedb.p.rapidapi.com/exercises', options)
-          .then(response => response.json())
-          .then(response => console.log(response))
-          .catch(err => console.error(err));
+fetch("https://exercisedb.p.rapidapi.com/exercises", options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
 
-
-
-
-
+  function showExercise(){
+    
+  }
---
+++ maps.js
@@ -0,0 +1,45 @@
+let map;
+
+function getLocation(city) {
+  if (city) {
+    //use the geo to get the lat and Long
+    fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${city}do&key=AIzaSyCOYW44XORsf-nBZKXvYwZ8VPxDIgq8X7w`,
+      {
+        method: "GET",
+        dataType: "jsonp",
+        headers: {},
+      }
+    )
+      .then((response) => response.json())
+      .then((city) => {
+        let lat = city.results[0].geometry.location.lat;
+        let long = city.results[0].geometry.location.lng;
+
+        // pass the Lat and Long to google place API
+        gMap(lat, long);
+        getGym(lat, long);
+      });
+  }
+}
+
+function showPosition(position) {
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+
+  gMap(lat, long);
+  getGym(lat, long);
+}
+
+function gMap(lat, long) {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: lat, lng: long },
+    zoom: 10,
+  });
+}
+
+document.querySelector(".citySearch").addEventListener("click", (e) => {
+  let city = document.getElementById("city").value;
+  console.log(city);
+  getLocation(city);
+});
