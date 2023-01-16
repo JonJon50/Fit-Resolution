01c40bb382ae12a67dc743f3f64e6e51196e64f0
MilenaSilveira <120193252+MilenaSilveira@users.noreply.github.com>
2023-01-15 19:24:25 -0500

    Added html content to exercise file

--- exercise.js
+++ exercise.js
@@ -1,12 +1,12 @@
 const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': 'dfbd975756msh5c34e78571a6059p156249jsne62ed8b440fa',
-        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-    }
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '21a4844f08msh3a160f840f7c4dap1e8cbbjsn27ab3c79b460',
+		'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+	}
 };
 
-fetch('https://exercisedb.p.rapidapi.com/exercises', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+fetch('https://exercisedb.p.rapidapi.com/exercises/exercise/%7Bid%7D', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
